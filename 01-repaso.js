a01abd9b615adb68a2378817378ed207055d9fe4
SteelFenix08 <f_771@outlook.com>
2024-08-22 14:41:47 -0600

    Tarea

---
+++ 01-repaso.js
@@ -0,0 +1,10 @@
+let A = [3,4,5,6]
+
+function Lectura (){
+    for ( let i = 0; i < A; i++){
+        let valores = A[i]
+        console.log(`Los valores del indice ${i} : con valor ${valores}`);
+    }
+}
+
+Lectura()
---
+++ 02-repaso.js
@@ -0,0 +1,31 @@
+//Realizar un areglo con 2 dimensiones y guardar dos areglos internos y dar lectura a los datos internos
+
+let cosa = [
+    [ 1, 2, 3],
+    [3, 5, 6]
+]
+
+cosa.map()
+
+
+// for of
+// for in
+// funcion flecha
+// while
+
+//Estructura de control parser
+
+let a = 4
+let b = 5
+
+function Decision (){ //funcion definida sin parametros
+/*     if (a>b){
+    console.log(`La a es mayor ${a}`);
+} else {
+    console.log(`La b es mayor ${b}`);
+} */
+
+    const  comprare = a<b?"A es mayor":"B es mayor"
+    console.log(comprare);
+} 
+
