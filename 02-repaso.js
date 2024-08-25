9024f3517cb0c68bfa2ae08e42dcac94a8f4fc60
SteelFenix09 <f_771@outlook.com>
2024-08-25 11:26:54 -0600

    Actualizacion

--- 02-repaso.js
+++ 02-repaso.js
@@ -1,31 +1,79 @@
 //Realizar un areglo con 2 dimensiones y guardar dos areglos internos y dar lectura a los datos internos
 
 let cosa = [
-    [ 1, 2, 3],
-    [3, 5, 6]
+    [1, 2, 3], [3, 5, 6]
 ]
 
-cosa.map()
+const areglo = () => {
+    cosa.map((datos, index) => {
+        datos.map((valores, i) => {
+            console.log(`${i}=${valores}`);
+
+        })
+    })
+}
+
 
 
 // for of
+const ciclo = () => {
+    for (let i of cosa) {
+        for (let j of i){
+            console.log(`${j}`);
+            
+        }
+    }
+}
+ciclo()
 // for in
+const guardado = () => {
+    for(let i in cosa){
+        let dado = cosa[i]
+        for(let j in dado){
+            console.log(`${j}`);
+            
+        }
+    }
+}
+guardado()
 // funcion flecha
-// while
+let flecha = () => {
+    for(let i=0 ; i < cosa.length ; i++){
+    let dados = cosa[i]
+    for(let j=0 ; j < dados.length ; j++){
+        console.log(dados[j]);
+    }
+}}
+flecha()
 
+// while
+let validar = () => {
+    let i = 0
+    while (i < cosa.length){
+        let j = 0
+        let dados = cosa[i]
+        while (j < dados.length) {
+            console.log(`${dados[j]}`);
+            j++
+        }
+        i++
+    }
+}
+validar()
 //Estructura de control parser
 
 let a = 4
 let b = 5
 
-function Decision (){ //funcion definida sin parametros
-/*     if (a>b){
-    console.log(`La a es mayor ${a}`);
-} else {
-    console.log(`La b es mayor ${b}`);
-} */
+function Decision() { //funcion definida sin parametros
+    /*     if (a>b){
+        console.log(`La a es mayor ${a}`);
+    } else {
+        console.log(`La b es mayor ${b}`);
+    } */
 
-    const  comprare = a<b?"A es mayor":"B es mayor"
+    const comprare = a < b ? "A es mayor" : "B es mayor"
     console.log(comprare);
-} 
+
+}
 
