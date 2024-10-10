a6e469cee05a9dc537099320514d73f4a15e6a50
SteelFenix09 <f_771@outlook.com>
2024-10-10 07:45:16 -0600

    Guardar

---
+++ todo/Sorpresa.js
@@ -0,0 +1,48 @@
+const prompt = require("prompt-sync")()
+
+
+class Persona {
+    constructor(nombre, edad, sexo, direccion, telefono) {
+        this._nombre = nombre
+        this._edad = edad
+        this._sexo = sexo
+        this._direccion = direccion
+        this._telefono = telefono
+        this._guardar = []
+    }
+
+    Almacen() {
+        this._guardar.forEach((mvp) => {
+            console.log(`Hola ${mvp.nombre}, tienes ${mvp.edad}, del genero ${mvp.sexo}
+            vives en ${mvp.direccion} y tu numero telofonico es ${this._telefono}`);
+        })
+
+
+    }
+
+    Datos() {
+        let total = 3
+
+        for (let i = 0; i <= total; i++) {
+            this._nombre = prompt("Ingrese su nombre ")
+            this._edad = parseInt(prompt("Ingrese su edad "))
+            this._sexo = prompt("Ingrese su genero ")
+            this._direccion = prompt("Ingrese su direccion ")
+            this._telefono = parseInt(prompt("Ingrese su numero telefonico "))
+
+            let cosas = {
+                nombre: this._nombre,
+                edad: this._edad,
+                sexo: this._sexo,
+                direccion: this._direccion,
+                telefono: this._telefono
+            }
+            this._guardar.push(cosas)
+        }
+
+    }
+}
+
+let otro = new Persona()
+otro.Datos()
+otro.Almacen()
